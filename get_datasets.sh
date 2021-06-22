6ae043081afe44bdf650547ff59d6711f3e18cba
Настя <49210968+NastyaTataurova@users.noreply.github.com>
2021-06-22 18:17:16 +0300

    Update get_datasets.sh

--- get_datasets.sh
+++ get_datasets.sh
@@ -1,26 +1,16 @@
 FILE=$1
-if [[$FILE == "edges2shoes"]]; then
-  URL=http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/$FILE.tar.gz
-  TAR_FILE=./datasets/$FILE.tar.gz
-  TARGET_DIR=./datasets/$FILE/
-  wget -N $URL -O $TAR_FILE
-  mkdir -p $TARGET_DIR
-  tar -zxvf $TAR_FILE -C ./datasets/
-  rm $TAR_FILE
-fi
 
-if [[$FILE == "maps"]]; then
-    echo "Specified [$FILE]"
-    URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
-    mkdir $datasets
-    ZIP_FILE=./$FILE.zip
-    TARGET_DIR=./$FILE/
-    wget -N $URL -O $ZIP_FILE
-    mkdir $TARGET_DIR
-    unzip $ZIP_FILE -d ./datasets/
-    rm $ZIP_FILE
-    rm -r /content/pix2pix_pytorch/datasets/maps/train/resized
-fi
+echo "Specified [$FILE]"
+URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
+mkdir $datasets
+ZIP_FILE=./$FILE.zip
+TARGET_DIR=./$FILE/
+wget -N $URL -O $ZIP_FILE
+mkdir $TARGET_DIR
+unzip $ZIP_FILE -d ./datasets/
+rm $ZIP_FILE
+rm -r /content/pix2pix_pytorch/datasets/maps/train/resized
+
 
 
 
