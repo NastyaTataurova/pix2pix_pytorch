94bbeaf3a521bf2e6e0c6fa44bc2af58b876fd42
Настя <49210968+NastyaTataurova@users.noreply.github.com>
2021-06-26 13:48:36 +0300

    Update get_edges2shoes_datasets.sh

--- get_edges2shoes_datasets.sh
+++ get_edges2shoes_datasets.sh
@@ -1,8 +1,8 @@
 URL=http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/edges2shoes.tar.gz
 TAR_FILE=./datasets/edges2shoes.tar.gz
 TARGET_DIR=./datasets/edges2shoes/
-wget -N $URL -O $TAR_FILE
 mkdir -p $TARGET_DIR
+wget -N $URL -O $TAR_FILE
 tar -zxvf $TAR_FILE -C ./datasets/
 rm $TAR_FILE
 rm ./datasets/edges2shoes/train/{2003..49001}_AB.jpg
