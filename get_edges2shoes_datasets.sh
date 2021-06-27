f6b6cd7be057c47e1f6791a07aac6c7bef75e213
Настя <49210968+NastyaTataurova@users.noreply.github.com>
2021-06-27 16:51:51 +0300

    Delete get_edges2shoes_datasets.sh

--- get_edges2shoes_datasets.sh
+++
@@ -1,8 +0,0 @@
-URL=http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/edges2shoes.tar.gz
-TAR_FILE=./datasets/edges2shoes.tar.gz
-TARGET_DIR=./datasets/edges2shoes/
-mkdir -p $TARGET_DIR
-wget -N $URL -O $TAR_FILE
-tar -zxvf $TAR_FILE -C ./datasets/
-rm $TAR_FILE
-rm ./datasets/edges2shoes/train/{2003..49001}_AB.jpg
