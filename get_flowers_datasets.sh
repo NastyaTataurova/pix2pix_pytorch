1190b112bfb97a9575573462a742bdf23565f628
Настя <49210968+NastyaTataurova@users.noreply.github.com>
2021-06-27 10:45:23 +0300

    Update get_flowers_datasets.sh

--- get_flowers_datasets.sh
+++ get_flowers_datasets.sh
@@ -3,7 +3,7 @@ TAR_FILE=./datasets/trimaps.tgz
 TARGET_DIR=./datasets/flowers/trimaps
 mkdir -p $TARGET_DIR
 wget -N $URL -O $TAR_FILE
-tar -zxvf $TAR_FILE -C ./datasets/trimaps/
+tar -zxvf $TAR_FILE -C ./datasets/flowers/trimaps/
 rm $TAR_FILE
 rm ./datasets/flowers/trimaps/trimaps/imlist.mat
 
@@ -13,6 +13,6 @@ TAR_FILE=./datasets/17flowers.tgz
 TARGET_DIR=./datasets/flowers/jpg
 mkdir -p $TARGET_DIR
 wget -N $URL -O $TAR_FILE
-tar -zxvf $TAR_FILE -C ./datasets/jpg/
+tar -zxvf $TAR_FILE -C ./datasets/flowers/jpg/
 rm $TAR_FILE
 rm ./datasets/flowers/jpg/jpg/files.txt
