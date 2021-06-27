a4c76a1ff7e9d4528948b272c83b55094aec4fee
Настя <49210968+NastyaTataurova@users.noreply.github.com>
2021-06-27 11:16:05 +0300

    Update get_flowers_datasets.sh

--- get_flowers_datasets.sh
+++ get_flowers_datasets.sh
@@ -1,18 +1,24 @@
 URL=https://www.robots.ox.ac.uk/~vgg/data/flowers/17/trimaps.tgz
-TAR_FILE=./datasets/flowers/trimaps.tgz
-TARGET_DIR=./datasets/flowers/trimaps
+TAR_FILE=./datasets/flowers/train/trimaps.tgz
+TARGET_DIR=./datasets/flowers/train/trimaps
+TEST_DIR=./datasets/flowers/test/trimaps/trimaps
 mkdir -p $TARGET_DIR
+mkdir -p $TEST_DIR
 wget -N $URL -O $TAR_FILE
-tar -zxvf $TAR_FILE -C ./datasets/flowers/trimaps/
+tar -zxvf $TAR_FILE -C ./datasets/flowers/train/trimaps/
 rm $TAR_FILE
-rm ./datasets/flowers/trimaps/trimaps/imlist.mat
+rm ./datasets/flowers/train/trimaps/trimaps/imlist.mat
+mv ./datasets/flowers/train/trimaps/trimaps/image-000{1..9}.jpg $TEST_DIR
 
 
 URL=https://www.robots.ox.ac.uk/~vgg/data/flowers/17/17flowers.tgz
-TAR_FILE=./datasets/flowers/17flowers.tgz
-TARGET_DIR=./datasets/flowers/jpg
+TAR_FILE=./datasets/flowers/train/17flowers.tgz
+TARGET_DIR=./datasets/flowers/train/jpg
+TEST_DIR=./datasets/flowers/test/jpg/jpg
 mkdir -p $TARGET_DIR
+mkdir -p $TEST_DIR
 wget -N $URL -O $TAR_FILE
-tar -zxvf $TAR_FILE -C ./datasets/flowers/jpg/
+tar -zxvf $TAR_FILE -C ./datasets/flowers/train/jpg/
 rm $TAR_FILE
-rm ./datasets/flowers/jpg/jpg/files.txt
+rm ./datasets/flowers/train/jpg/jpg/files.txt
+mv ./datasets/flowers/train/jpg/jpg/image-000{1..9}.jpg $TEST_DIR
