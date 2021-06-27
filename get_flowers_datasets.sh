4aa3efe4753f408b7f0cc98b43bfbf45ed24ee5b
Настя <49210968+NastyaTataurova@users.noreply.github.com>
2021-06-27 10:36:57 +0300

    Update get_flowers_datasets.sh

--- get_flowers_datasets.sh
+++ get_flowers_datasets.sh
@@ -1,10 +1,18 @@
-URL=http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/edges2shoes.tar.gz https://www.robots.ox.ac.uk/~vgg/data/flowers/17/trimaps.tgz
+URL=https://www.robots.ox.ac.uk/~vgg/data/flowers/17/trimaps.tgz
 TAR_FILE=./datasets/trimaps.tgz
 TARGET_DIR=./datasets/flowers/trimaps
 mkdir -p $TARGET_DIR
 wget -N $URL -O $TAR_FILE
 tar -zxvf $TAR_FILE -C ./datasets/
 rm $TAR_FILE
-/content/trimaps/imlist.mat
+rm ./datasets/flowers/trimaps/trimaps/imlist.mat
 
-jpg /content/jpg/files.txt
+
+URL=https://www.robots.ox.ac.uk/~vgg/data/flowers/17/17flowers.tgz
+TAR_FILE=./datasets/17flowers.tgz
+TARGET_DIR=./datasets/flowers/jpg
+mkdir -p $TARGET_DIR
+wget -N $URL -O $TAR_FILE
+tar -zxvf $TAR_FILE -C ./datasets/
+rm $TAR_FILE
+rm ./datasets/flowers/jpg/jpg/files.txt
