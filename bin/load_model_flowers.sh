86fe39df9130841f8f16203aee3f1c37da54eec1
Настя <49210968+NastyaTataurova@users.noreply.github.com>
2021-06-28 12:43:56 +0300

    Update load_model_flowers.sh

--- bin/load_model_flowers.sh
+++ bin/load_model_flowers.sh
@@ -1,3 +1,3 @@
-pip install -q -U --no-cache-dir gdown --pre 
-gdown --id 1h4fLxg9e4sYTdi_eniiP5CTHPKtOZM1O 
-gdown --id 1h5Hk2h9aik16ZFvMaRWNkO9shQaqmiMU 
+pip install -q -U --no-cache-dir gdown --pre #
+gdown --id 1h4fLxg9e4sYTdi_eniiP5CTHPKtOZM1O #
+gdown --id 1h5Hk2h9aik16ZFvMaRWNkO9shQaqmiMU #
