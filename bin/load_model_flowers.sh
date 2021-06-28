7c84a95b7e5dbc3289c7d9b3e064535d84f5f8df
Настя <49210968+NastyaTataurova@users.noreply.github.com>
2021-06-28 11:57:17 +0300

    Update load_model_flowers.sh

--- bin/load_model_flowers.sh
+++ bin/load_model_flowers.sh
@@ -1,4 +1,5 @@
-cd ./pix2pix_pytorch # loading the weights of a model trained on a dataset with flowers
+cd ./pix2pix_pytorch
+# loading the weights of a model trained on a dataset with flowers
 pip install -q -U --no-cache-dir gdown --pre
 gdown --id 1h4fLxg9e4sYTdi_eniiP5CTHPKtOZM1O
 gdown --id 1h5Hk2h9aik16ZFvMaRWNkO9shQaqmiMU
