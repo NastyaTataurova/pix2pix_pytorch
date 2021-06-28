917340a689795a7cd3dc130715bb740d68b1a7b9
Настя <49210968+NastyaTataurova@users.noreply.github.com>
2021-06-28 15:28:53 +0300

    Update generate_maps.py

--- crs/generate_maps.py
+++ crs/generate_maps.py
@@ -1,6 +1,11 @@
 from pix2pix_pytorch.crs.model.generator import *
 from pix2pix_pytorch.crs.model.discriminator import *
 from pix2pix_pytorch.crs.utils.save_load_weights import *
+import os
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import torchvision.transforms as tt
 
 
 if __name__ == "__main__":
@@ -22,6 +27,24 @@ if __name__ == "__main__":
     load_model_optimazer('gener_maps.pth', gener_maps, optimizer_gener_maps, lr, device)
 
 
-    print('файлы есть вот тут /home/nastya/photo.png')
-    dir = input("напши дир с файлом") #/home/nastya/photo.png
-    #TODO
+    print('Загрузка файла. Файл есть вот тут: <./images/map.jfif>')
+    file_path = input('Напишите путь файлу: ')
+
+    image_size = 256
+    # stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+    transforms = tt.Compose([tt.ToPILImage(),
+                             tt.Resize((256, 256)),
+                             tt.ToTensor(),
+                             # tt.Normalize(*stats)
+                             ])
+
+    # image generation
+    image = Image.open(file_path)
+    image = transforms(np.array(image))
+
+    gener_image = gener_maps(image.unsqueeze(0).to(device))[0]
+    gener_image = np.asarray(gener_image.permute(1, 2, 0).cpu().detach().numpy(), dtype=np.float32)
+
+    # result
+    im = Image.fromarray((gener_image * 255).astype(np.uint8))
+    im.show()
