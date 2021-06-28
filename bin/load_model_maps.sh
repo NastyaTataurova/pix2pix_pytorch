d119ae07283b362cb75a92cfbe0a0a22869587b6
Настя <49210968+NastyaTataurova@users.noreply.github.com>
2021-06-28 10:42:26 +0300

    Update load_model_maps.sh

--- bin/load_model_maps.sh
+++ bin/load_model_maps.sh
@@ -1,6 +1,6 @@
+cd ./pix2pix_pytorch
+
 # loading the weights of a model trained on a dataset with maps
 pip install -q -U --no-cache-dir gdown --pre
 gdown --id 1FNSg9qF8LMaI5GujFl4vyfYnf0j8JVSW
 gdown --id 17vBZy44-irz4OAfnJ00q-nujyeYjTBrW
-
-cd ./pix2pix_pytorch
